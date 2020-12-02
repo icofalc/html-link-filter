7a710445a13bd871706a00b05d1bc639c2ddb120
icofalc <38294321+icofalc@users.noreply.github.com>
2020-12-02 09:26:56 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,25 @@
+import bs4
+import glob
+import os
+import os.path
+
+d = os.getcwd()
+for root, dirs, files in os.walk(d):
+    for file in files:
+        if file.endswith('.htm'):
+
+            filei = str(file)
+            with open(filei, encoding="utf8") as f:
+                print("\n\n\n")
+                print(filei)
+                print("\n\n\n")
+                soup = bs4.BeautifulSoup(f,features="html.parser")
+                mydivs = soup.findAll('a')
+                for link in mydivs:
+                    print(str(link) + "\n")
+
+
+
+    
+    
+    
